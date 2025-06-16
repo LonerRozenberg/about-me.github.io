118c0d9b184e3e3ba576084ffc4d3ea28935d1ab
LonerRozenberg <lonerrozenberg@gmail.com>
2025-06-16 15:02:17 +0600

    deploy add 4

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ npm run build
 # Переходим в папку сборки
 cd dist
 
+# Если вы деплоите в project pages (username.github.io/repo-name)
+echo 'about-me.github.io' > CNAME
+
 # Инициализируем Git и пушим на GitHub
 git init
 git checkout -b main
